177bff8cc55f4dfc78276bb252b4246c02c73ae9
Ilya-Belyanov <belyanov.99@mail.ru>
2020-06-20 22:11:52 +0300

    Delete comments

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from PyQt5 import QtWidgets
 from Connect import Connector
 import sys
-"""
-To Do
-1) Collision
-2) Reflection
-3) Two player
-"""
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     application = Connector()
